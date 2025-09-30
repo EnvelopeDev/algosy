d09d38184d3a5bcbdee73996d8327d155b7be965
Daunito228 <rep.gep1945@gmail.com>
2025-10-01 02:22:48 +0300

    some fixes

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,10 +1,5 @@
 #include "setTest.hpp"
-#include "generator.hpp" //generation of test cases (groups of 4 sets - A, B, C, D)
 #include "consoleInterface.hpp"
-
-void inputFromConsole(); //does the input from the console to input.csv
-std::vector<std::string> inputGroupSet(); //does the input from console to the set group (A, B, C, D)
-bool isValidSet(std::string str); //for checking correctness of the entered set
 
 int main(){
     ConsoleInterface consoleInterface;
