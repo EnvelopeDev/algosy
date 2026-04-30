28f7ea2579a6643ca03bd7799af1db7ba51c771e
Daunito228 <rep.gep1945@gmail.com>
2026-04-30 16:01:12 +0300

    added unique types key-value

--- lab7/HashTable.cpp
+++
@@ -1,9 +0,0 @@
-#include "HashTable.hpp"
-
-HashTable::HashTable():numBuckets(43){
-
-}
-
-unsigned HashTable::hash(const BitMaskSet& set){
-    return set.getMask()%numBuckets;
-}
--- lab7/HashTable.hpp
+++ lab7/HashTable.hpp
@@ -1,15 +1,23 @@
 #pragma once
 #include <vector>
 #include <list>
+#include <functional>
+
 #include "bmask_set.hpp"
 
+template<typename TK, typename TV>
 class HashTable
 {
 private:
     int numBuckets;
-    std::vector<std::list<std::pair<unsigned, BitMaskSet>>> buckets; //Вектор со списками в каждом из которых хранятся пары ключ-значение
-    unsigned hash(const BitMaskSet& set);
+    std::vector<std::list<std::pair<TK, TV>>> buckets; //Вектор со списками в каждом из которых хранятся пары ключ-значение
+    std::hash<TK> hasher;
 public:
-    HashTable();
+    HashTable(const size_t _numBuckets=43);
     ~HashTable();
 };
+
+template<typename TK, typename TV>
+HashTable<TK, TV>::HashTable(const size_t _numBuckets=43):numBuckets(_numBuckets){
+    buckets.resize(numBuckets);
+}
