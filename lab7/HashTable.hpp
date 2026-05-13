f4c541c8b5384dcfe66fffa69cb3848e67880ca5
Daunito228 <rep.gep1945@gmail.com>
2026-05-13 16:15:24 +0300

    insert, get, contain

--- lab7/HashTable.hpp
+++ lab7/HashTable.hpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <list>
 #include <functional>
+#include <stdexcept>
 
 #include "bmask_set.hpp"
 
@@ -18,6 +19,8 @@ public:
     ~HashTable();
 
     void insert(const TK& key, const TV& value);
+    TV& getElem(const TK& key);
+    bool contains(const TK& key);
 };
 
 template<typename TK, typename TV>
@@ -36,5 +39,42 @@ unsigned HashTable<TK, TV>::getBucketIndex(const TK& key){
 template<typename TK, typename TV>
 void HashTable<TK, TV>::insert(const TK& key, const TV& value){
     unsigned bucketIndex = getBucketIndex(key);
-    buckets[bucketIndex].push_back();
+
+    for(auto& elem:buckets[bucketIndex]){
+        if(elem.first==key){
+            elem.second=value;
+            return;
+        }
+    }
+
+    if(buckets[bucketIndex].empty()){
+        buckets[bucketIndex].push_back(std::make_pair(key, value));
+    }
+}
+
+template<typename TK, typename TV>
+TV& HashTable<TK, TV>::getElem(const TK& key){
+    if(!contains(key)){
+        throw std::invalid_argument("NO SUCH KEY!\n");
+    }
+    unsigned bcktIndex = getBucketIndex(key);
+    TV& value=(*buckets[bcktIndex].begin()).second;
+    for(const auto& elem:buckets[bcktIndex]){
+        if(elem.first==key){
+            value = elem.second;
+            break;
+        }
+    }
+    return value;
+}
+
+template<typename TK, typename TV>
+bool HashTable<TK, TV>::contains(const TK& key){
+    unsigned bcktIndex = getBucketIndex(key);
+    for(const auto& elem:buckets[bcktIndex]){
+        if(elem.first==key){
+            return true;
+        }
+    }
+    return false;
 }
--- lab7/Interface.cpp
+++ lab7/Interface.cpp
@@ -6,13 +6,16 @@ Interface::~Interface(){}
 
 void Interface::show(){
     std::cout << "МНОЖЕСТВО СОСТОИТ ИЗ ВСЕХ ЗАГЛАВНЫХ ЛАТИНСКИХ БУКВ И СТРОЧНЫХ ДО f ВКЛЮЧИТЕЛЬНО" << std::endl;
-    HashTable<std::string, BitMaskSet> ht;
+    HashTable<std::string, int> ht;
     BitMaskSet bset;
     Generator gen;
     std::string s("HelloWorld");
-    for(int i=0;i<10;i++){
-        bset.setMask(gen.generateSet(4, 32));
-        ht.insert(s, bset);
-        s[0]++;
-    }
+    ht.insert(s, 60);
+    ht.insert("1234", 50);
+    ht.insert("HLWD", 40);
+    std::cout << ht.getElem("1234")<<std::endl;
+    ht.insert("1234", 55);
+    std::cout << ht.getElem("1234")<<std::endl;
+    std::cout << ht.getElem("HelloWorld")<<std::endl;
+    std::cout << ht.getElem("HLWD")<<std::endl;
 }
